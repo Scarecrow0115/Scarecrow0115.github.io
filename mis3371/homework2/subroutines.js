e7f27b61a1600e6ed6ed8398926a75ec3830cba5
Scarecrow0115 <69991020+Scarecrow0115@users.noreply.github.com>
2024-10-16 15:18:56 -0500

    Update subroutines.js

--- mis3371/homework2/subroutines.js
+++ mis3371/homework2/subroutines.js
@@ -3,18 +3,18 @@ Program name: subroutines.js
 Author: Christian Loup
 Date created: 10-16-2024
 Date last edited: 10-16-2024
-Version: 1.01
+Version: 1.02
 Description: subroutines for patient form.
 */
-document.getElementById('passwordForm').addEventListener('submit', function(event) {
+document.getElementById('homework2form').addEventListener('submit', function(event) {
   const password = document.getElementById('password').value;
-  const confirmPassword = document.getElementById('confirmPassword').value;
-  const errorMessage = document.getElementById('error-message');
+  const repassword = document.getElementById('repassword').value;
+  const pwdError = document.getElementById('pwdError');
 
-if (password !== confirmPassword) {
-  errorMessage.textContent = "Passwords do not match.";
-  event.preventDefault(); // Prevent form submission
-} else {
-  errorMessage.textContent = ""; // Clear error message
-}
+  if (password !== repassword) {
+    pwdError.textContent = "Passwords do not match.";
+    event.preventDefault(); // Prevent form submission
+  } else {
+    pwdError.textContent = ""; // Clear error message
+  }
 });
