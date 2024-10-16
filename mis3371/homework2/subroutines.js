f684f42708fdb4c851508aaa14664f00917fb733
Scarecrow0115 <69991020+Scarecrow0115@users.noreply.github.com>
2024-10-16 17:08:43 -0500

    Update subroutines.js

--- mis3371/homework2/subroutines.js
+++ mis3371/homework2/subroutines.js
@@ -3,7 +3,7 @@ Program name: subroutines.js
 Author: Christian Loup
 Date created: 10-16-2024
 Date last edited: 10-16-2024
-Version: 1.04
+Version: 1.05
 Description: subroutines for patient form.
 */
 document.getElementById('homework2form').addEventListener('submit', function(event) {
@@ -22,19 +22,15 @@ document.getElementById('homework2form').addEventListener('submit', function(eve
 document.getElementById('homework2form').addEventListener('submit', function(event) {
   const selectedDate = new Date(document.getElementById('bdate').value);
   const currentDate = new Date();
-
+  const tooOld = new Date();
   currentDate.setHours(0, 0, 0, 0);
+  tooOld.setFullYear(currentDate.getFullYear() - 120);
 
   if (selectedDate > currentDate) {
     alert("Birthday can not be in the future.");
     event.preventDefault(); // prevent submission
-  }
-
-  const tooOld = new Date();
-  tooOld.setFullYear(currentDate.getFullYear() - 120);
-
-  if (selectedDate < tooOld) {
-    alert("Birthday can not be more than 120 years ago.");
-    event.preventDefault(); // Prevent form submission
+  } else if (selectedDate < tooOld) {
+      alert("Birthday can not be more than 120 years ago.");
+      event.preventDefault(); // Prevent form submission
   }
 });
