0538e4bc1b2992476de86e68678b755b09774cef
Scarecrow0115 <69991020+Scarecrow0115@users.noreply.github.com>
2024-10-16 15:08:12 -0500

    Update subroutines.js

--- mis3371/homework2/subroutines.js
+++ mis3371/homework2/subroutines.js
@@ -1,4 +1,11 @@
-//
+/*
+Program name: subroutines.js
+Author: Christian Loup
+Date created: 10-16-2024
+Date last edited: 10-16-2024
+Version: 1.01
+Description: subroutines for patient form.
+*/
 document.getElementById('passwordForm').addEventListener('submit', function(event) {
   const password = document.getElementById('password').value;
   const confirmPassword = document.getElementById('confirmPassword').value;
