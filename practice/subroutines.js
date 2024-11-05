538a01aca087e2135122e03ad04f5eb29cec1398
Scarecrow0115 <69991020+Scarecrow0115@users.noreply.github.com>
2024-11-05 13:38:45 -0600

    Update subroutines.js

--- practice/subroutines.js
+++ practice/subroutines.js
@@ -84,7 +84,8 @@ document.addEventListener("DOMContentLoaded", () => {
     function validateSocial() {
         const social = document.getElementById("ss");
         const error = document.getElementById("ssError");
-        const regex = /^[0-9]{9,11}$|^$/;
+        //const regex = /^[0-9]{9,11}$|^$/;
+        const regex = /^(|[0-9]{9,11})$/;
 
         if (!regex.test(social.value)) {
             error.textContent = "Invalid Social Security number. 9-11 numbers only.";
