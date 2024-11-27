9e678aaa32b25464fbe2bec6a8bc8f6bda2e40bc
Scarecrow0115 <69991020+Scarecrow0115@users.noreply.github.com>
2024-11-27 10:28:49 -0600

    Update subroutines.js

--- mis3371/homework4/subroutines.js
+++ mis3371/homework4/subroutines.js
@@ -2,13 +2,47 @@
 Program name: subroutines.js
 Author: Christian Loup
 Date created: 11-24-2024
-Date last edited: 11-24-2024
-Version: 1.01
+Date last edited: 11-27-2024
+Version: 1.02
 Description: subroutines for patient form.
 */
 
-// Cookies
-document.cookie = "fname=" + document.getElementById("fname").value;
+// Cookie functions
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  let username = getCookie("username");
+  if (username != "") {
+   alert("Welcome again " + username);
+  } else {
+    username = prompt("Please enter your name:", "");
+    if (username != "" && username != null) {
+      setCookie("username", username, 365);
+    }
+  }
+}
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("homework4form");
