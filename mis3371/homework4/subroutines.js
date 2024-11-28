30199ac5f29b5d049ab2a45e9e06d3fc385ddb71
Scarecrow0115 <69991020+Scarecrow0115@users.noreply.github.com>
2024-11-27 23:26:50 -0600

    Update subroutines.js

--- mis3371/homework4/subroutines.js
+++ mis3371/homework4/subroutines.js
@@ -3,7 +3,7 @@ Program name: subroutines.js
 Author: Christian Loup
 Date created: 11-24-2024
 Date last edited: 11-27-2024
-Version: 1.04
+Version: 1.05
 Description: subroutines for patient form.
 */
 
@@ -43,7 +43,7 @@ function checkCookie() {
   }
 }
 
-function clearCookie() {
+function clearCookie(cname) {
   document.cookie = cname + "=; expires=Mon, 01 Jan 2024 00:00:00 UTC; path=/;";
 }
 
